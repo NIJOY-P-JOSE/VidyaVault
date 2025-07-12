6919bcf78821cb7d9e757d0e5b6b20f18e903239
Nijoy P Jose <160378117+NIJOY-P-JOSE@users.noreply.github.com>
2025-07-12 12:57:50 +0530

    Update settings.py
    Change DATABASE

--- vidyavault/settings.py
+++ vidyavault/settings.py
@@ -3,6 +3,7 @@ Django settings for vidyavault project.
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 from dotenv import load_dotenv
@@ -73,11 +74,12 @@ WSGI_APPLICATION = 'vidyavault.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
